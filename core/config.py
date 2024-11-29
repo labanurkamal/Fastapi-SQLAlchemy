e9bd3d2cc40ae521929ddc4e11454d2952a2360f
Aibek <154282821+labanurkamal@users.noreply.github.com>
2024-11-29 05:24:06 +0500

    Add package models -> base.py with Base class, core -> config.py with Settings class.

---
+++ core/config.py
@@ -0,0 +1,11 @@
+from dotenv import find_dotenv
+
+from pydantic_settings import BaseSettings, SettingsConfigDict
+
+
+class Settings(BaseSettings):
+    model_config = SettingsConfigDict(env_file=find_dotenv(), extra="ignore")
+    db_url: str = "sqlite+aiosqlite:///./db.sqlite3"
+
+
+settings = Settings()
