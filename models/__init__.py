a0849d98339af572c26cd5cd2e57cd515cab3884
Aibek <154282821+labanurkamal@users.noreply.github.com>
2024-12-03 06:57:53 +0500

    Add class DatabaseHelper that helper configuration sqlalchemy.

---
+++ models/__init__.py
@@ -0,0 +1,10 @@
+__all__ = (
+    "Base",
+    "DatabaseHelper",
+    "db_helper",
+    "Product",
+)
+
+from models.base import Base
+from models.db_helper import DatabaseHelper, db_helper
+from models.product import Product
