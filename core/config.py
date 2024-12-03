aecb549f9e87122385dd2ae1a8c510c6e8f45e17
Aibek <154282821+labanurkamal@users.noreply.github.com>
2024-12-03 06:51:16 +0500

    Addition settings sqlalchemy conf, api_v1_prefix.

--- core/config.py
+++ core/config.py
@@ -1,10 +1,17 @@
+from pathlib import Path
 from dotenv import find_dotenv
+
 from pydantic_settings import BaseSettings, SettingsConfigDict
+
+BASE_DIR = Path(__file__).parent.parent
 
 
 class Settings(BaseSettings):
     model_config = SettingsConfigDict(env_file=find_dotenv(), extra="ignore")
-    db_url: str = "sqlite+aiosqlite:///./db.sqlite3"
+    api_v1_prefix: str = "/api/v1"
+    db_url: str = f"sqlite+aiosqlite:///{BASE_DIR}/db.sqlite3"
+    # db_echo: bool = False
+    db_echo: bool = True
 
 
 settings = Settings()
