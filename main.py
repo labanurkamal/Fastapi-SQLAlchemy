3dfa7427505151ce989b1447ad4138a5c4253c7d
Aibek <154282821+labanurkamal@users.noreply.github.com>
2024-12-03 07:01:48 +0500

    Addition main.py.

--- main.py
+++ main.py
@@ -1,9 +1,22 @@
+from contextlib import asynccontextmanager
+
 from fastapi import FastAPI
 
+from api.v1 import router as router_v1
+from models import Base, db_helper
 from items import router as item_router
 from users.views import router as users_router
+from core.config import settings
 
-app = FastAPI()
+
+@asynccontextmanager
+async def lifespan(app: FastAPI):
+    async with db_helper.engine.begin() as conn:
+        await conn.run_sync(Base.metadata.create_all)
+    yield
+
+
+app = FastAPI(lifespan=lifespan)
 
 
 @app.get("/")
@@ -16,5 +29,6 @@ def hello(name: str = "World"):
     return {"message": f"Hello, {name.title()}"}
 
 
+app.include_router(router=router_v1, prefix=settings.api_v1_prefix)
 app.include_router(users_router, prefix="/users", tags=["USERS"])
 app.include_router(item_router, prefix="/items", tags=["ITEMS"])
